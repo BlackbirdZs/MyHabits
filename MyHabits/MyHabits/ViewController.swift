3a0641be8d251b19bf1ad1d5cc99e2567e1f1804
Acronym <clearsky61@gmail.com>
2026-09-14 16:52:21 +0300

    new project start

---
+++ MyHabits/MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Anton Kruglov on 14.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
